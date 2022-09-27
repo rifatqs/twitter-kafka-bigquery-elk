e0b03a8fc5312fa9630b742789d2e0e66a072e54
rifatqs <110024371+rifatqs@users.noreply.github.com>
2022-09-27 20:20:21 +0700

    Update elastic-consum.py

--- consumer/elastic-consum.py
+++ consumer/elastic-consum.py
@@ -8,7 +8,7 @@ es = Elasticsearch("http://localhost:9200")
 
 consumer = KafkaConsumer(
     'twitter',
-    bootstrap_servers=['35.240.249.157:9092'],
+    bootstrap_servers=['(broker):9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='euy',
