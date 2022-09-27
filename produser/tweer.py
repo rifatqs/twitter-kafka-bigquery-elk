dfee8013c11e9c1c9068b91ff1e31d0f616d3d60
rifatqs <110024371+rifatqs@users.noreply.github.com>
2022-09-27 20:22:49 +0700

    Update tweer.py

--- produser/tweer.py
+++ produser/tweer.py
@@ -1,10 +1,10 @@
 import tweepy
 
 
-access_token ="264630885-y2nJBVa6r5FfbcksmrO6Ixx1IodT6ZtZf2M29eQx"
-acces_token_secret = "DL64aX0uelQLJwA9dYYbocCFycekdfQ37xO4aESziGo8J"
-api_key = "2wWuJZcgdwM9VaO0l0nmoGVw5"
-api_key_secret ="GwHxNyiHzVq6uP9Brvp0JIW0pQSDJ7YBnf6hodO5b3hqKBfONY"
+access_token ="..."
+acces_token_secret = "..."
+api_key = "..."
+api_key_secret ="..."
 
 auth = tweepy.OAuthHandler(api_key,api_key_secret)
 auth.set_access_token(access_token,acces_token_secret)
